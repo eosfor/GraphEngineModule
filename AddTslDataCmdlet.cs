6e0b60f44d8969261218284df2e42b55335edb42
Andrey Vernigora <andrey_vernigora@epam.com>
2018-04-26 14:16:20 +0300

    first commit

---
+++ AddTslDataCmdlet.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Management.Automation;
+using Trinity;
+using Trinity.Storage.Composite;
+
+namespace GraphEngineMolule
+{
+    [Cmdlet("Add", "TslData")]
+    public class AddTslDataCmdlet : PSCmdlet
+    {
+        [Parameter()]
+        public string Path;
+
+        [Parameter()]
+        public string Namespace;
+
+
+        protected override void ProcessRecord()
+        {
+            Global.Initialize();
+            CompositeStorage.AddStorageExtension(Path, Namespace);
+
+            //base.ProcessRecord();
+        }
+    }
+}
---
+++ SetConfigOptionCmdlet.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using System.Management.Automation;
+using Trinity;
+using Trinity.Storage.Composite;
+using Trinity.Configuration;
+using System.Reflection;
+
+namespace GraphEngineMolule
+{
+    [Cmdlet("Set", "ConfigOption")]
+    public class SetConfigOptionCmdlet : PSCmdlet
+    {
+        [Parameter(Mandatory = false)]
+        public string LogDirectory;
+
+        [Parameter(Mandatory = false)]
+        public Trinity.Diagnostics.LogLevel LogLevel;
+
+        [Parameter(Mandatory = false)]
+        public string StorageRoot;
+
+        protected override void BeginProcessing()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += CurrentDomain_BindingRedirect;
+
+            base.BeginProcessing();
+        }
+
+        protected override void ProcessRecord()
+        {
+            //Global.Initialize();
+
+            if (this.MyInvocation.BoundParameters.ContainsKey("LogLevel"))
+            {
+                TrinityConfig.LoggingLevel = LogLevel;
+            }
+
+            if (this.MyInvocation.BoundParameters.ContainsKey("LogDirectory"))
+            {
+                if (Directory.Exists(LogDirectory))
+                {
+                    LoggingConfig.Instance.LogDirectory = LogDirectory;
+                }
+                else { throw new DirectoryNotFoundException(); }
+
+            }
+
+            if (this.MyInvocation.BoundParameters.ContainsKey("StorageRoot"))
+            {
+                if (Directory.Exists(StorageRoot))
+                {
+                    StorageConfig.Instance.StorageRoot = StorageRoot;
+                }
+                else { throw new DirectoryNotFoundException(); }
+
+            }
+            //base.ProcessRecord();
+        }
+
+
+        public static Assembly CurrentDomain_BindingRedirect(object sender, ResolveEventArgs args)
+
+        {
+
+            var name = new AssemblyName(args.Name);
+            switch (name.Name)
+            {
+                case "Newtonsoft.Json":
+                    return typeof(Newtonsoft.Json.JsonSerializer).Assembly;
+                case "Trinity.Core":
+                    return Assembly.LoadFrom(@"C:\Repo\Github\Public\00 - MSFT\GraphEngine\bin\netstandard2.0\Trinity.Core.dll");
+                default:
+                    return null;
+            }
+
+        }
+    }
+}
