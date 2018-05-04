b545f78e931683ddae66e4e42b4b1725fa1539d1
Andrey Vernigora <andrey_vernigora@epam.com>
2018-05-04 17:45:00 +0300

    some updates

--- Tests/tests.ps1
+++ Tests/tests.ps1
@@ -7,6 +7,43 @@ invoke-pester ..\..\..\tests
 Set-GEConfigOption -LogDirectory "C:\Tests\psTrinityTests\storage\trinity-log" -StorageRoot "C:\Tests\psTrinityTests\storage" -LogEchoOnConsole $false
 Add-GETslData -Path C:\Repo\Github\My\GraphEngineModule\TSL\ -Namespace tsltest
 
-$cell = [tsltest.VNET]::new()
-Add-GEVertex -Vertex $cell
+<#
+		g
+		|
+	a-b-c-f
+	  | |
+	  d h
+	  |
+	  e
+#>
+
+
+$a = [tsltest.node]::new("A")
+$b = [tsltest.node]::new("B")
+$c = [tsltest.node]::new("C")
+$d = [tsltest.node]::new("D")
+$e = [tsltest.node]::new("E")
+$f = [tsltest.node]::new("F")
+$g = [tsltest.node]::new("G")
+$h = [tsltest.node]::new("H")
+
+Add-GEVertex -Vertex $a
+Add-GEVertex -Vertex $b
+Add-GEVertex -Vertex $c
+Add-GEVertex -Vertex $d
+Add-GEVertex -Vertex $e
+Add-GEVertex -Vertex $f
+Add-GEVertex -Vertex $g
+Add-GEVertex -Vertex $h
+
 Get-GECellCount
+
+Add-GEEdge -from $a -to $b
+Add-GEEdge -from $b -to $c
+Add-GEEdge -from $c -to $f
+
+Add-GEEdge -from $b -to $d
+Add-GEEdge -from $d -to $e
+
+Add-GEEdge -from $c -to $h
+Add-GEEdge -from $c -to $g
